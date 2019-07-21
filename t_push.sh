5d4d9e179c5b6f83d7ee1b9fc2b086b506dfc26d
Andrew Eggleston <egglestonandrew927@gmail.com>
2019-07-21 18:08:26 -0400

    Update t_push.sh

--- t_push.sh
+++ t_push.sh
@@ -5,17 +5,16 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
-  git checkout -b gh-pages
-  git add . *.html
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+commit_files() {
+  git add *.pdf
+  git commit --message "[travis skip] Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin https://${GH_TOKEN}@github.com/andreweggleston/cv.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
 }
 
 setup_git
-commit_website_files
+commit_files
 upload_files
